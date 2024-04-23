36b359a22d73e98a655d875d2ab1e10309a47dd0
Bruno@Garcia <bgconcept.web@gmail.com>
2024-04-23 12:27:56 +0200

    Dots

--- ReadMe.md
+++ ReadMe.md
@@ -1,2 +1,12 @@
-# Print-It WebSite v.1
 https://codepen.io/luxonauta/pen/QWEWvmB
+
+https://www.youtube.com/watch?v=gRsbdNu_8SU
+
+////////////////////////////////////
+Références des événements
+https://developer.mozilla.org/fr/docs/Web/Events
+
+///////////////////////////////////
+Javascript pour inserer du contenu dans le document html.
+https://www.youtube.com/watch?v=oibheNXmUg8&list=PLQkrqTy7RjTHPfe7PDJpDgScj6TPTZWr5&index=6
+
--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,86 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+console.log(slides);
+
 //selction des differents elements
+
 //Images
-const bannerImg = document.querySelector('.banner-img');
+const bannerImg = document.querySelector(".banner-img");
+console.log("banner-img");
+//TagLine
+const tagLine = document.querySelector(["tagLine"])
 //Flèches
-const arrowLeft=document.querySelector(".arrow_left");
-const arrowRight=document.querySelector(".arrow_right");
-//Points
-const dot=document.querySelector(".dot");
+const arrowLeft = document.querySelector(".arrow_left");
+const arrowRight = document.querySelector(".arrow_right");
+console.log("arrowLeft");
+console.log("arrowRight");
+
+//Dots
+let dots = document.querySelector(".dots");
+let dotSelected = document.querySelector(".dot_selected")
+let html = `
+	<div class="dot_selected"></div>
+		<div class="dot"></div>
+		<div class="dot"></div>	
+		<div class="dot"></div>	
+		<div class="dot"></div>	
+	
+ `
+dots.innerHTML = html;
+
+/*Côté animation*/
+
+// index actuel de la position slide
+let currentIndex = 0;
+
+// index actuel de la position span
+let position = 0;
 
 // boucle for
-for (let index =0; index < slides.length; index++){
+for (let index = 0; index < slides.length; index++) {
 	console.log(slides[index])
 }
 slides.forEach(slide => {
 	console.log(slide);
-})
 
-let position = 0;
+});
 
 
-arrowLeft.addEventListener("click", function(){
-	position --;
-	if(position < 0){
-		position = slides.length - 1;
+//FLECHE GAUCHE
+// arrowLeft.addEventListener("click", () => {
+// 	position--;
+
+// 	if (position < 0) {
+// 		position = slides.length - 1;
+// 	}
+
+
+// })
+
+ //FLECHE DROITE
+ function clickRight(){
+	 arrowRight.addEventListener("click", () => {
+		 position++;
+		 image.src = slides[currentIndex].imageSrc;
+		 
+		});
 	}
-	console.log(slides[position])
-
-})
-
-arrowRight.addEventListener("click", function(){
-	position ++;
-	if(position >= slides.length){
-		position = 0;
-	}
-	console.log(slides[position])
-})
+clickRight();
 
 
